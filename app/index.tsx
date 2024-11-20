871b56fb6f83d53118a1e474e85460d7c2ae419f
Dev Bryan <bryandpc07@gmail.com>
2024-11-20 19:54:02 -0300

    Primeiro commit

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <View style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <Text style={styles.subtitle}>Socorro.</Text>
       </View>
     </View>
   );
