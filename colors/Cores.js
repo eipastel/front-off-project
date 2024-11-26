1e4831c281d24f51042661db9c4a4644d37d0d3d
eipastel <gaitadothiago@gmail.com>
2024-11-26 20:08:15 -0300

    FIX removendo pasta do node modules

--- colors/Cores.js
+++
@@ -1,9 +0,0 @@
-export const Cores = {
-  padroes: {
-    azulClaro: '#A9CEF4',
-    verdeEscuro: '#69995D',
-    preto: '#1D201F',      
-    vermelho: '#FF6B6B',   
-    roxoEscuro: '#413C58',
-  },
-};
